95d502d1052298e4fca3d3cd904885891a8cb5e6
wangwj <wangwj@wangwj-K40IN.(none)>
2019-03-03 12:17:01 +0800

    add clear_list(),create_list() in list.c

--- list/list.c
+++ list/list.c
@@ -11,22 +11,23 @@ typedef struct _LIST {
     struct _LIST *next;
 } stList;
 
+
 void print_list(stList *pHead)
 {
-    stList *pTemp = NULL;
+    printf("%s enter\n", __FUNCTION__);
     if (NULL == pHead) {
         printf("pHead is null\n");
         return;
     }
 
-    pTemp = pHead;
-    while (NULL != pTemp) {
-    printf("ix=%d ", pTemp->key);
-    pTemp = pTemp->next;
+    while (NULL != pHead) {
+        printf("ix=%d, pHead=%p ,pHead->next=%p\n", pHead->key, pHead, pHead->next);
+        pHead = pHead->next;
    }
-   printf("\n");
+   printf("%s END\n", __FUNCTION__);
    return;
 }
+
 void invert_list(stList **pHead) {
     stList *pT = NULL;
     stList *pTail = NULL;
@@ -46,24 +47,72 @@ void invert_list(stList **pHead) {
     return;
 }
 
-void main() {
-    stList *pHead = NULL;
-    int ix;
+void clear_list(stList* pHead) {
     stList *pTemp = NULL;
+    printf("%s enter\n", __FUNCTION__);
+    if (NULL == pHead) {
+        printf("pHead is null\n");
+        return;
+    }
 
+    while (NULL != pHead) {
+        pTemp = pHead->next;
+        pHead->next = NULL;
+        printf("Now clear %d, pHead:%p, pTemp:%p \n", pHead->key, pHead, pTemp);
+        pHead->key = 0x0;
+        free(pHead);
+        pHead = pTemp;
+    }
+    pTemp = NULL;
+    pHead = NULL;
+    printf("%s end\n", __FUNCTION__);
+}
+
+void create_list(stList **ppHead) {
+    int ix = 0;
+    stList *pTemp = NULL;
+    stList *pHead = NULL;
+    if (NULL == ppHead) {
+        printf("ppHead is null\n");
+        return;
+    }
+    if (NULL != *ppHead) {
+        printf("*ppHead is not null\n");
+        return;
+    }
     for (ix=0; ix<N; ix++) {
-        printf("\n %d> pHead=%p",ix,  pHead);
+        printf("create %d> pHead=%p \n",ix, pHead);
         pTemp = (stList *)malloc(sizeof(stList));
         pTemp->key = ix;
         pTemp->next = pHead;
         pHead = pTemp;
         pTemp = NULL;
-        printf(" pHead=%p, next=%p", pHead, pHead->next);
+    }
+    *ppHead = pHead; //do not forget to get the addr
+    printf("%s end\n", __FUNCTION__);
+}
+
+void main() {
+    int ix;
+    stList *pHead = NULL;
+#if 0    
+    stList *pTemp = NULL;
+    for (ix=0; ix<N; ix++) {
+        printf("create %d> pHead=%p \n",ix,  pHead);
+        pTemp = (stList *)malloc(sizeof(stList));
+        pTemp->key = ix;
+        pTemp->next = pHead;
+        pHead = pTemp;
         pTemp = NULL;
     }
-    printf("\n");
-
+#endif
+    create_list(&pHead);
     print_list(pHead);
+#if 0
     invert_list(&pHead); 
-    print_list(pHead);    
+    print_list(pHead);
+#endif
+    clear_list(pHead);
+    pHead = NULL; // prevent pHead is wild pointer
+    print_list(pHead); 
 }

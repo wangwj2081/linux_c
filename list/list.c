083ddf4fd9d1eab8eb5c7b1e3084bc05e613a55e
wangwj <wangwj@wangwj-K40IN.(none)>
2019-03-11 23:00:49 +0800

    add remove_list_node()

--- list/list.c
+++ list/list.c
@@ -3,7 +3,7 @@
 #include <string.h>
 
 #define N 10
-#define TEST_MSG_ERROR(fmt, ...) fprintf(stderr, "VT_ERROR %s::%d " fmt"\n",__FUNCTION__, __LINE__,## __VA_ARGS__)
+#define PRINT_MSG(fmt, ...) printf("%s::%d " fmt"\n", __FUNCTION__, __LINE__, ## __VA_ARGS__)
 #define print(fm, ...) printf(fm"line:%d \n", ##__VA_ARGS__, __LINE__)
 
 typedef struct _LIST {
@@ -21,10 +21,11 @@ void print_list(stList *pHead)
     }
 
     while (NULL != pHead) {
-        printf("ix=%d, pHead=%p ,pHead->next=%p\n", pHead->key, pHead, pHead->next);
+        // printf("ix=%d, pHead=%p ,pHead->next=%p\n", pHead->key, pHead, pHead->next);
+        printf("%d ", pHead->key);
         pHead = pHead->next;
    }
-   printf("%s END\n", __FUNCTION__);
+   printf("\n %s END\n", __FUNCTION__);
    return;
 }
 
@@ -58,20 +59,64 @@ void clear_list(stList* pHead) {
     while (NULL != pHead) {
         pTemp = pHead->next;
         pHead->next = NULL;
-        printf("Now clear %d, pHead:%p, pTemp:%p \n", pHead->key, pHead, pTemp);
+        //printf("Now clear %d, pHead:%p, pTemp:%p \n", pHead->key, pHead, pTemp);
+        printf("%d ", pHead->key);
         pHead->key = 0x0;
         free(pHead);
         pHead = pTemp;
     }
     pTemp = NULL;
     pHead = NULL;
-    printf("%s end\n", __FUNCTION__);
+    printf("\n %s end\n", __FUNCTION__);
+}
+#if 0
+void remove_list(stList **ppHead, int count) {
+    stList *pt = *ppHead;
+    stList *pre = NULL;
+    while (count != pt->key) {
+        printf("now is %d\n", pt->key);
+        pre = pt;
+        pt = pt->next;
+    }
+    printf("now find it:%d\n", pt->key);
+    if (pt->key == (*ppHead)->key) {
+        printf("should remove the head\n");
+        //ppHead = &(pt->next); it is wrong
+        *ppHead = pt->next;
+    } else {
+       printf("should remove the others\n");
+       pre->next = pt->next;
+    }
+    printf("pt = %p\n", pt);
+    printf("pt->next = %p\n", pt->next);
+    printf("*ppHead = %p\n", *ppHead);
+   // pt->next = NULL;
+   // free(pt);
+    pt = NULL;
+    pre = NULL;  
+}
+#endif
+void remove_list(stList **ppHead, int count) {
+    //stList **ppLink = NULL;
+    //*ppLink = *ppHead;
+    stList *pt = *ppHead;
+    printf("pt = %p\n", pt);
+    while (pt->key != count) {
+        ppHead = &(pt->next);
+        pt = *ppHead;
+    }
+
+    *ppHead = pt->next;
+    pt->next = NULL;
+    free(pt);
 }
 
-void create_list(stList **ppHead) {
+void create_list(stList **ppHead, int num) {
     int ix = 0;
     stList *pTemp = NULL;
     stList *pHead = NULL;
+
+    printf("%s enter, create %d node\n", __FUNCTION__, num);
     if (NULL == ppHead) {
         printf("ppHead is null\n");
         return;
@@ -80,8 +125,8 @@ void create_list(stList **ppHead) {
         printf("*ppHead is not null\n");
         return;
     }
-    for (ix=0; ix<N; ix++) {
-        printf("create %d> pHead=%p \n",ix, pHead);
+    for (ix=0; ix<num; ix++) {
+       //printf("create %d> pHead=%p \n",ix, pHead);
         pTemp = (stList *)malloc(sizeof(stList));
         pTemp->key = ix;
         pTemp->next = pHead;
@@ -93,20 +138,14 @@ void create_list(stList **ppHead) {
 }
 
 void main() {
-    int ix;
+    int num = 12;
     stList *pHead = NULL;
-#if 0    
-    stList *pTemp = NULL;
-    for (ix=0; ix<N; ix++) {
-        printf("create %d> pHead=%p \n",ix,  pHead);
-        pTemp = (stList *)malloc(sizeof(stList));
-        pTemp->key = ix;
-        pTemp->next = pHead;
-        pHead = pTemp;
-        pTemp = NULL;
-    }
-#endif
-    create_list(&pHead);
+    
+    create_list(&pHead, num);
+    PRINT_MSG("pHead=%p\n", pHead);
+    print_list(pHead);
+    remove_list(&pHead, 9);
+    PRINT_MSG("pHead=%p\n", pHead);
     print_list(pHead);
 #if 0
     invert_list(&pHead); 
